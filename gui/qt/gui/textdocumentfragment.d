bb1196d7f7d7ae443a89277c963c84c2c75a33a8
Tim Schendekehl <tim.dlang@t-online.de>
2021-12-11 19:58:29 +0100

    Use non-Ddoc comment for license

--- gui/qt/gui/textdocumentfragment.d
+++ gui/qt/gui/textdocumentfragment.d
@@ -1,16 +1,14 @@
-/****************************************************************************
-**
-** DQt - D bindings for the Qt Toolkit
-**
-** GNU Lesser General Public License Usage
-** This file may be used under the terms of the GNU Lesser
-** General Public License version 3 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL3 included in the
-** packaging of this file. Please review the following information to
-** ensure the GNU Lesser General Public License version 3 requirements
-** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
-**
-****************************************************************************/
+/*
+ * DQt - D bindings for the Qt Toolkit
+ *
+ * GNU Lesser General Public License Usage
+ * This file may be used under the terms of the GNU Lesser
+ * General Public License version 3 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.LGPL3 included in the
+ * packaging of this file. Please review the following information to
+ * ensure the GNU Lesser General Public License version 3 requirements
+ * will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+ */
 module qt.gui.textdocumentfragment;
 extern(C++):
 
