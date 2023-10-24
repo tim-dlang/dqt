8c609ceb9b75a663878cdc3d5aa34b1ce84de543
Tim Schendekehl <tim.dlang@t-online.de>
2023-10-24 18:23:53 +0200

    Add linker option -headerpad_max_install_names on OSX

--- runtests.d
+++ runtests.d
@@ -296,6 +296,7 @@ int main(string[] args)
             else version(OSX)
             {
                 dmdArgs ~= "-L-F" ~ qtPath ~ "/lib";
+                dmdArgs ~= "-L-headerpad_max_install_names";
             }
             else
             {
