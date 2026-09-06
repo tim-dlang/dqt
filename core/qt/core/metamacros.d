b3ff66ee929120715e42e72a96277a9f6ac05e8b
Tim Schendekehl <tim.dlang@t-online.de>
2026-09-06 20:24:26 +0200

    Fix QMetaMethod.methodType for generated meta object

--- core/qt/core/metamacros.d
+++ core/qt/core/metamacros.d
@@ -517,6 +517,7 @@ template MetaObjectImpl(T)
                 size_t parameterCount = Parameters!(M[i]).length;
                 uint flags;
                 flags |= 2; // Public // TODO
+                flags |= type;
                 metaDataCode ~= mixin(interpolateMixin(q{
                        $(text(nameId)),    $(text(parameterCount)),   $(text(currentOutputIndex)),    2, $(text(flags)), $("// " ~ __traits(identifier, M[i]))
                 }));
--- tests/testcore1.d
+++ tests/testcore1.d
@@ -563,6 +563,7 @@ unittest
     assert(method.name().toConstCharArray() == "signalVoid");
     assert(method.methodSignature().toConstCharArray() == "signalVoid()");
     assert(method.parameterCount() == 0);
+    assert(method.methodType() == QMetaMethod.MethodType.Signal);
 
     method = mo.method(mo.methodOffset() + 1);
     assert(method.name().toConstCharArray() == "signalInt");
@@ -632,6 +633,18 @@ unittest
     assert(method.name().toConstCharArray() == "signalCustomFlags");
     assert(method.methodSignature().toConstCharArray() == "signalCustomFlags(CustomFlags)");
     assert(method.parameterCount() == 1);
+
+    method = mo.method(mo.indexOfMethod("emitSignalVoid()"));
+    assert(method.name().toConstCharArray() == "emitSignalVoid");
+    assert(method.methodSignature().toConstCharArray() == "emitSignalVoid()");
+    assert(method.parameterCount() == 0);
+    assert(method.methodType() == QMetaMethod.MethodType.Method);
+
+    method = mo.method(mo.indexOfMethod("onSignalVoid()"));
+    assert(method.name().toConstCharArray() == "onSignalVoid");
+    assert(method.methodSignature().toConstCharArray() == "onSignalVoid()");
+    assert(method.parameterCount() == 0);
+    assert(method.methodType() == QMetaMethod.MethodType.Slot);
 
     method = mo.method(mo.methodOffset() + numSignalSlotTests * 3 + 4);
     assert(method.name().toConstCharArray() == "toUpper");
